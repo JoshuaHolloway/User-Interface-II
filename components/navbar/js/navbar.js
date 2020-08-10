1eabb908e091569cb4c4d5f7a37c731bc815933f
JoshuaHolloway <jhollow6@asu.edu>
2020-08-10 17:53:19 -0500

    wired up buttons for opening and closing the collapsed navbar

--- components/navbar/js/navbar.js
+++ components/navbar/js/navbar.js
@@ -1,9 +1,11 @@
 const openSlideMenu = () => {
     document.getElementById('Side-Menu').style.width = '250px';
-    document.getElementById('Main').style.marginLeft = '250px';
+    // document.getElementById('Main').style.marginLeft = '250px';
+    console.log('.Side-Menu pressed');
 }
 
 const closeSlideMenu = () => {
-    document.getElementById('Side-Menu').style.width = '0px';
-    document.getElementById('Main').style.marginLeft = '0px';
+    // document.getElementById('Side-Menu').style.width = '0px';
+    // document.getElementById('Main').style.marginLeft = '0px';
+    console.log('.Btn-Close pressed');
 }
