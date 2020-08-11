c6f0bc1b95c46ac8940d9a9e5adb92f3d7b7880d
JoshuaHolloway <jhollow6@asu.edu>
2020-08-10 19:32:51 -0500

    desired functionality on the red and blue div.

--- components/theme-selector/js/theme-selector.js
+++ components/theme-selector/js/theme-selector.js
@@ -1,8 +1,4 @@
-const theme_color = document.querySelector('.Color-Options-Container > .Active-Theme');
 
-console.log('josh');
-console.log(theme_color);
-console.log(theme_color.id);
 
 
 // Set CSS varaible values with JS:
@@ -20,25 +16,48 @@ const set_theme = (color) => {
     const str = `--background-color-${color}`;
     const new_css_var_val = get_css_var_val(str);
     set_css_var_val('--background-color-primary', new_css_var_val);
-    body.style.backgroundColor = get_css_var_val('--background-color-primary');
+    document.querySelector('body').style.backgroundColor = get_css_var_val('--background-color-primary');
+    // console.log('new_css_var_val = ' + new_css_var_val);
+    // console.log('--background-color-primary = ' + get_css_var_val('--background-color-primary'));
 };
 
-const body = document.querySelector('body');
 const theme_buttons = Array.from(document.querySelectorAll('.Color-Options-Container > button'));
 console.log(theme_buttons);
 theme_buttons.forEach(item => {
     item.addEventListener('click', event => {
-        if (theme_color.id === 'Theme-Blue') {
+
+        // -A button has been pressed.
+        // -Disable current active classes, and set the specific one afterwards
+        // theme_buttons.forEach(item => {
+        //     item.classList.toggle('');
+        // });
+
+        //Toggle currently active theme:
+        console.log('About to toggle:');
+        const current_active_theme_color_elem = document.querySelector('.Color-Options-Container > .Active-Theme');
+        console.log('current_active_theme_color_elem:');
+        console.log(current_active_theme_color_elem);
+        current_active_theme_color_elem.classList.toggle('Active-Theme');
+
+
+        //console.log('theme_color.id = ' + theme_color.id);
+
+
+
+        if (current_active_theme_color_elem.id === 'Theme-Blue') {
             set_theme('blue');
 
             // Set active class
-            item.classList.toggle('Active-Theme');
+            //item.classList.toggle('Active-Theme');
+            // const theme_color = document.querySelector('.Color-Options-Container > .Active-Theme');
+            // console.log('josh');
+            // console.log(theme_color);
+            // console.log(theme_color.id);
             // TODO: Toggle this off when you select another element!
             // TODO: Toggle this off when you select another element!           
-        } else if (theme_color.id === 'Theme-Yellow') {
-            // don't do anything yet!!!
-
-            body.style.backgroundColor = get_css_var_val('--background-color-primary');
+        } else if (current_active_theme_color_elem.id === 'Theme-Yellow') {
+            console.log('Theme-Yellow');
+            set_theme('yellow');
         }
     });
 });
