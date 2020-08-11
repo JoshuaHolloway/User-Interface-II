d08d64f899022f5054e4151dc3145949c43d087e
JoshuaHolloway <jhollow6@asu.edu>
2020-08-10 19:11:31 -0500

    theme picker component. Setting background color dynamically based on user interaction.

--- components/theme-selector/js/theme-selector.js
+++ components/theme-selector/js/theme-selector.js
@@ -16,26 +16,30 @@ const set_css_var_val = (css_var_name, new_val) => {
     document.documentElement.style
         .setProperty(css_var_name, new_val);
 };
+const set_theme = (color) => {
+    const str = `--background-color-${color}`;
+    const new_css_var_val = get_css_var_val(str);
+    set_css_var_val('--background-color-primary', new_css_var_val);
+    body.style.backgroundColor = get_css_var_val('--background-color-primary');
+};
 
-
+const body = document.querySelector('body');
 const theme_buttons = Array.from(document.querySelectorAll('.Color-Options-Container > button'));
 console.log(theme_buttons);
 theme_buttons.forEach(item => {
     item.addEventListener('click', event => {
         if (theme_color.id === 'Theme-Blue') {
-            const new_css_var_val = get_css_var_val('--background-color-blue');
-            set_css_var_val('--background-color-primary', new_css_var_val);
+            set_theme('blue');
 
             // Set active class
             item.classList.toggle('Active-Theme');
-
             // TODO: Toggle this off when you select another element!
-            // TODO: Toggle this off when you select another element!
-            // TODO: Toggle this off when you select another element!
-            // TODO: Toggle this off when you select another element!
-            // TODO: Toggle this off when you select another element!
-            
+            // TODO: Toggle this off when you select another element!           
         } else if (theme_color.id === 'Theme-Yellow') {
+            // don't do anything yet!!!
+
+            body.style.backgroundColor = get_css_var_val('--background-color-primary');
+        }
     });
 });
 
