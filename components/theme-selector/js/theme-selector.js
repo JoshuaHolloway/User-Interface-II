22c3a50efebd839a3b4af81e32c6f0ed4ef93c6c
JoshuaHolloway <jhollow6@asu.edu>
2020-08-10 20:37:37 -0500

    color theme picker basically complete

--- components/theme-selector/js/theme-selector.js
+++ components/theme-selector/js/theme-selector.js
@@ -1,65 +1,61 @@
-
-
-
 // Set CSS varaible values with JS:
 // https://davidwalsh.name/css-variables-javascript#:~:text=To%20set%20the%20value%20of,style%20.
-const get_css_var_val = (css_var_name) => {
-    const css_var_val = getComputedStyle(document.documentElement)
-    .getPropertyValue(css_var_name);
-    return css_var_val;
-};
-const set_css_var_val = (css_var_name, new_val) => {
-    document.documentElement.style
-        .setProperty(css_var_name, new_val);
-};
-const set_theme = (color) => {
-    const str = `--background-color-${color}`;
-    const new_css_var_val = get_css_var_val(str);
-    set_css_var_val('--background-color-primary', new_css_var_val);
-    document.querySelector('body').style.backgroundColor = get_css_var_val('--background-color-primary');
-    // console.log('new_css_var_val = ' + new_css_var_val);
-    // console.log('--background-color-primary = ' + get_css_var_val('--background-color-primary'));
+const get_css_vals = (color) => {
+    const val = (var_name) => getComputedStyle(document.documentElement)
+    .getPropertyValue(var_name);
+
+    const background = val(`--background-color-${color}`);
+    const border = val(`--border-color-${color}`);
+    const glow = val(`--glow-color-${color}`);
+    return [background, border, glow];
 };
 
-const theme_buttons = Array.from(document.querySelectorAll('.Color-Options-Container > button'));
-console.log(theme_buttons);
-theme_buttons.forEach(item => {
-    item.addEventListener('click', event => {
+const elem = name => document.getElementById(name);
+const theme_1 = elem('theme-1');
+const blue = elem('blue');
+const body = elem('body');
 
-        // -A button has been pressed.
-        // -Disable current active classes, and set the specific one afterwards
-        // theme_buttons.forEach(item => {
-        //     item.classList.toggle('');
-        // });
+const setup_theme_callback = (num, color) => {
+    elem(`theme-${num}`).addEventListener('click', () => {
+        [background, border, glow] = get_css_vals(color);
+        body.style.backgroundColor = background;
 
-        //Toggle currently active theme:
-        console.log('About to toggle:');
-        const current_active_theme_color_elem = document.querySelector('.Color-Options-Container > .Active-Theme');
-        console.log('current_active_theme_color_elem:');
-        console.log(current_active_theme_color_elem);
-        current_active_theme_color_elem.classList.toggle('Active-Theme');
+        // this.style.boxShadow = '0 0 10px 4px var(--glow-color-primary);';
 
-
-        //console.log('theme_color.id = ' + theme_color.id);
-
-
-
-        if (current_active_theme_color_elem.id === 'Theme-Blue') {
-            set_theme('blue');
-
-            // Set active class
-            //item.classList.toggle('Active-Theme');
-            // const theme_color = document.querySelector('.Color-Options-Container > .Active-Theme');
-            // console.log('josh');
-            // console.log(theme_color);
-            // console.log(theme_color.id);
-            // TODO: Toggle this off when you select another element!
-            // TODO: Toggle this off when you select another element!           
-        } else if (current_active_theme_color_elem.id === 'Theme-Yellow') {
-            console.log('Theme-Yellow');
-            set_theme('yellow');
+        // Turn off all other active-classes:
+        for(let i = 1; i <= 8; ++i) {
+            elem(`theme-${i}`).classList.remove('Active-Theme');
+            // elem(`theme-${num}`).style.boxShadow = `none`;
         }
-    });
-});
+            
+
+        // Now set the single active-class:
+        console.log(elem(`theme-${num}`));
+        elem(`theme-${num}`).classList.toggle('Active-Theme');
+                                // box-shadow: none|h-offset v-offset blur spread color |inset|initial|inherit;
+        // elem(`theme-${num}`).style.boxShadow = `0 0 40px 16px var(--glow-color-${color})`;
+        // const active = document.querySelector('.Active-Theme');
+        // console.log(active);
+        // active.style.boxShadow = `0 0 10px 4px var(--glow-color-${color})`;
+
+        const set_css_var_val = (css_var_name, new_val) => {
+            document.documentElement.style
+                .setProperty(css_var_name, new_val);
+        };
+        const get_css_var_val = (css_var_name) => {
+            const css_var_val = getComputedStyle(document.documentElement)
+            .getPropertyValue(css_var_name);
+            return css_var_val;
+        };
+
+        set_css_var_val(`--glow-color-primary`, glow);
+        console.log('--glow-color-primary: ' + get_css_var_val('--glow-color-primary'));
+
+    });            
+};
+
+const colors = ['blue', 'yellow', 'pink', 'green', 'purple', 'tan', 'grey', 'brown'];
 
 
+for (let i = 1; i <= colors.length; ++i)
+    setup_theme_callback(i, colors[i - 1]);
